68d5ccf21b67a7347750570e26f627862a61d836
Hugh Donkin and Shawna Duvall <shawna@>
2019-06-05 09:04:27 -0700

    creates base file structure

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+var x = parseInt(prompt("Enter a number"));
+
+var addTen = function(input){
+  var y = 10
+  var result = input + y
+  return result;
+
+};
+
+var addTwenty = function(input){
+  var y = 20
+  return input + y
+
+};
+
+console.log(addTen(x));
+console.log(addTwenty(x));
+console.log(result);
